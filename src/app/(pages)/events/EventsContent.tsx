2cdcc9bb71ae30bf34e8e12bac9665768ca4cbab
navin <95286412+sahnavin123@users.noreply.github.com>
2024-03-13 23:33:46 +0530

    removed comments

--- src/app/(pages)/events/EventsContent.tsx
+++ src/app/(pages)/events/EventsContent.tsx
@@ -12,22 +12,16 @@ const EventsContent = () => {
       spacing={10}
       m={"8rem"}
     >
-      {/* <Grid container>
-        <Grid item xs={6}> */}
       <Image
         src={require("../../../helper/events/Ghaziabad-district-championship.jpg")}
         style={{ height: "80vh", width: "auto" }}
         alt={""}
       />
-      {/* </Grid>
-        <Grid item xs={6}> */}
       <Image
         style={{ height: "80vh", width: "auto" }}
         src={require("../../../helper/events/referee-seminar.jpg")}
         alt={""}
       />
-      {/* </Grid>
-      </Grid> */}
     </Stack>
   );
 };
