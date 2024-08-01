69560e8a5ede266f44cfe04325979c84e54e0459
navin <95286412+sahnavin123@users.noreply.github.com>
2024-08-01 23:39:35 +0530

    updated event

--- src/app/(pages)/events/EventsContent.tsx
+++ src/app/(pages)/events/EventsContent.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Grid, Stack } from "@mui/material";
+import { Grid, Stack, Typography } from "@mui/material";
 import Image from "next/image";
 import React, { useState } from "react";
 
@@ -8,26 +8,136 @@ const EventsContent = () => {
   const [isHovered2, setIsHovered2] = useState(false);
 
   return (
-    <Stack
-      direction={{ sx: "column", md: "row" }}
-      alignItems={"center"}
-      justifyContent={"center"}
-      m={"8rem"}
-      gap={10}
-    >
-      <Image
-        src={require("../../../helper/events/event.jpg")}
-        style={{
-          height: isHovered1 ? "70vh" : "60vh",
-          width: "auto",
-          borderRadius: "1.5rem",
-          transition: "height 0.5s ease", // Adding smooth transition
-        }}
-        alt={""}
-        onMouseEnter={() => setIsHovered1(true)}
-        onMouseLeave={() => setIsHovered1(false)}
-      />
-      {/* <Image
+    <Stack>
+      <Stack gap={4}>
+        <Typography color="white" variant="h3" textAlign="center">
+          Latest Events
+        </Typography>
+        <Stack
+          flexWrap="wrap"
+          direction="row"
+          gap={2}
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Image
+            src={"/event/0.jpg"}
+            width={250}
+            height={250}
+            style={{
+              borderRadius: "1.5rem",
+              transition: "height 0.5s ease", // Adding smooth transition
+            }}
+            alt={""}
+            onMouseEnter={() => setIsHovered1(true)}
+            onMouseLeave={() => setIsHovered1(false)}
+          />
+          <Image
+            src={"/event/1.jpg"}
+            width={250}
+            height={250}
+            style={{
+              borderRadius: "1.5rem",
+              transition: "height 0.5s ease", // Adding smooth transition
+            }}
+            alt={""}
+            onMouseEnter={() => setIsHovered1(true)}
+            onMouseLeave={() => setIsHovered1(false)}
+          />
+          <Image
+            src={"/event/2.jpg"}
+            width={250}
+            height={250}
+            style={{
+              borderRadius: "1.5rem",
+              transition: "height 0.5s ease", // Adding smooth transition
+            }}
+            alt={""}
+            onMouseEnter={() => setIsHovered1(true)}
+            onMouseLeave={() => setIsHovered1(false)}
+          />
+          <Image
+            src={"/event/5.jpg"}
+            width={250}
+            height={250}
+            style={{
+              borderRadius: "1.5rem",
+              transition: "height 0.5s ease", // Adding smooth transition
+            }}
+            alt={""}
+            onMouseEnter={() => setIsHovered1(true)}
+            onMouseLeave={() => setIsHovered1(false)}
+          />
+          <Image
+            src={"/event/7.jpg"}
+            width={250}
+            height={250}
+            style={{
+              borderRadius: "1.5rem",
+              transition: "height 0.5s ease", // Adding smooth transition
+            }}
+            alt={""}
+            onMouseEnter={() => setIsHovered1(true)}
+            onMouseLeave={() => setIsHovered1(false)}
+          />
+          <Image
+            src={"/event/10.jpg"}
+            width={250}
+            height={250}
+            style={{
+              borderRadius: "1.5rem",
+              transition: "height 0.5s ease", // Adding smooth transition
+            }}
+            alt={""}
+            onMouseEnter={() => setIsHovered1(true)}
+            onMouseLeave={() => setIsHovered1(false)}
+          />
+          <Image
+            src={"/event/27.jpg"}
+            width={250}
+            height={250}
+            style={{
+              borderRadius: "1.5rem",
+              transition: "height 0.5s ease", // Adding smooth transition
+            }}
+            alt={""}
+            onMouseEnter={() => setIsHovered1(true)}
+            onMouseLeave={() => setIsHovered1(false)}
+          />
+          <Image
+            src={"/event/29.jpg"}
+            width={250}
+            height={250}
+            style={{
+              borderRadius: "1.5rem",
+              transition: "height 0.5s ease", // Adding smooth transition
+            }}
+            alt={""}
+            onMouseEnter={() => setIsHovered1(true)}
+            onMouseLeave={() => setIsHovered1(false)}
+          />
+        </Stack>
+      </Stack>
+      <Stack
+        direction={{ sx: "column", md: "row" }}
+        alignItems={"center"}
+        justifyContent={"center"}
+        m={"8rem"}
+        gap={10}
+      >
+        <Image
+          src={require("../../../helper/events/event.jpg")}
+          style={{
+            height: isHovered1 ? "70vh" : "60vh",
+            width: "auto",
+            borderRadius: "1.5rem",
+            transition: "height 0.5s ease", // Adding smooth transition
+          }}
+          alt={""}
+          onMouseEnter={() => setIsHovered1(true)}
+          onMouseLeave={() => setIsHovered1(false)}
+        />
+        {/* <Image
         style={{
           height: isHovered2 ? "80vh" : "70vh",
           width: "auto",
@@ -39,6 +149,7 @@ const EventsContent = () => {
         onMouseEnter={() => setIsHovered2(true)}
         onMouseLeave={() => setIsHovered2(false)}
       /> */}
+      </Stack>
     </Stack>
   );
 };
